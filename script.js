2c4c7ce207cdbb19bfeae6ffad3a754efeaa3149
Marcelo <marcebq2005@gmail.com>
2024-06-09 21:33:44 -1200

    third commit

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-let menuVisible = false;
-//Función que oculta o muestra el menu
-function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList ="";
-        menuVisible = false;
-    }else{
-        document.getElementById("nav").classList ="responsive";
-        menuVisible = true;
-    }
-}
+const buttonDescargar = document.querySelector("descargar");
 
-function seleccionar(){
-    //oculto el menu una vez que selecciono una opcion
+let menuVisible = false;
+
+function mostrarOcultarMenu() {
+  if (menuVisible) {
     document.getElementById("nav").classList = "";
     menuVisible = false;
+  } else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
 }
 
+function seleccionar() {
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
+}
 
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
-window.onscroll = function(){
-    efectoHabilidades();
-} 
+function descargarPDF() {
+    // Crear un elemento <a> para la descarga
+    const downloadInstance = document.createElement("a");
+    
+    // Establecer la URL del archivo PDF
+    downloadInstance.href = "https://marcebq.github.io/portfolio-1/CV_BARRIENTOS_QUISPE_MARCELO.pdf";
+    
+    // Especificar que se abra en una nueva pestaña
+    downloadInstance.target = "_blank";
+    
+    // Especificar el nombre del archivo para descargar
+    downloadInstance.download = "CV_BARRIENTOS_QUISPE_MARCELO.pdf";
+
+    // Simular un clic en el enlace para iniciar la descarga
+    downloadInstance.click();
+    
+    // Eliminar el elemento <a> después de la descarga
+    downloadInstance.remove();
+}
+
+  
+window.onscroll = function () {
+  efectoHabilidades();
+};
